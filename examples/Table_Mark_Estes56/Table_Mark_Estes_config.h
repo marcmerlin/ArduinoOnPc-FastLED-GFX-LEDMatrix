82c8770565a90cd167cdaa8c39ec1516c0746938
Marc MERLIN <marc_soft@merlins.org>
2021-01-12 22:34:46 -0800

    Update config.

--- examples/Table_Mark_Estes56/Table_Mark_Estes_config.h
+++ examples/Table_Mark_Estes56/Table_Mark_Estes_config.h
@@ -18,6 +18,7 @@
 // Comment this out to get all the patterns -- merlin
 #define BESTPATTERNS
 #ifdef BESTPATTERNS
+// 4 and 189 are similar
 // 16 and 17 are similar     
 // 55 and 102 are similar, 112 is somewhat similar
 // 66 and 115 are similar, 115 has stars in addition
@@ -30,7 +31,6 @@
 // 132 134 are similar, 134 looks better with reflections, 192 almost same
 // 150 and 185 and 186 are almost the same
 uint8_t bestpatterns[] = {
-    // 3: circles in circles
     // 14: circles around circles
     // 23: dragged rgb triangle: fix  to be triangle spin only, remove text and mirror
     // 26: lots of circles
@@ -49,12 +49,12 @@ uint8_t bestpatterns[] = {
     // 162: 2 crossed color waves. Fix: remove text and mirror
     // 187: big discs merging like shutter
     // 189: zoom circles. fix: remove text
-    3, 14, 23, 26, 69, 82, 87, 103, 110, 111, 115, 124, 134, 146, 150, 155, 162, 187, 189, // new patterns to add to FastLED_IR
-    // update code in code in 4 + 19 + 67
+    14, 23, 26, 69, 82, 87, 103, 110, 111, 115, 124, 134, 146, 150, 155, 162, 187, 189, // new patterns to add to FastLED_IR
+    // 4: different from old, 189 is similar
     // 52, 60, 62 are  different demo in new tme, don't want it. 105 changed a lot, I like the old one
     4, 10, 11, 25, 52, 60, 62, 67, 70, 77, 80, 104, 105,  // good in original tmed
 
-    17, 19, 20, 45, 53, 55, 58, 59, 66, 72, 73, 75, 79, 81, 86, 88, 89, 94, 96, 98, 101, 102, 107, 108, 109, 112, 113, 114, 118, 119, 120, 121, 126, 128, 131, 139, 141, 143, 144, 145, 147, 149, 151, 158, 192,   // good but not picked for FastLED_IR
+    3, 17, 19, 20, 45, 53, 55, 58, 59, 66, 72, 73, 75, 79, 81, 86, 88, 89, 94, 96, 98, 101, 102, 107, 108, 109, 112, 113, 114, 118, 119, 120, 121, 126, 128, 131, 139, 141, 143, 144, 145, 147, 149, 151, 158, 192,   // good but not picked for FastLED_IR
 
 // Aurora does not work well at resolutions over 256 wide or high or more than 64K pixels.
 // My RPI3 has a lower dispay and the RPI4 has a display that is too big
